5c4d3aa94bfa7d244e1dba2fad63234f1bcf38b2
fatemehkarami <karami.f@gmail.com>
2023-06-30 22:50:58 +1000

    first commit

--- README.md
+++ README.md
@@ -1,70 +1,43 @@
-# Getting Started with Create React App
+# Contact-Finder
 
-This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
+The Contact-Finder is a simple web application built with React that allows users to search and view contact information.
 
-## Available Scripts
+## Features
 
-In the project directory, you can run:
+- Search contacts by name
+- View contact details including name, email, and phone number
+- Dynamic dropdown menue
 
-### `npm start`
+## Technologies Used
 
-Runs the app in the development mode.\
-Open [http://localhost:3000](http://localhost:3000) to view it in your browser.
+- React
+- JavaScript
+- HTML
+- CSS
+- Bootstrap
 
-The page will reload when you make changes.\
-You may also see any lint errors in the console.
+## Getting Started
 
-### `npm test`
+To get started with the Contact-Finder, follow these steps:
 
-Launches the test runner in the interactive watch mode.\
-See the section about [running tests](https://facebook.github.io/create-react-app/docs/running-tests) for more information.
+1. Clone the repository to your local machine.
+2. Navigate to the project directory.
+3. Run the following command to install the dependencies:
+   npm install
+4. Once the dependencies are installed, start the development server:
+   npm start
 
-### `npm run build`
+5. Open your web browser and visit [http://localhost:3000](http://localhost:3000) to access the Contact-Finder.
 
-Builds the app for production to the `build` folder.\
-It correctly bundles React in production mode and optimizes the build for the best performance.
+## Usage
 
-The build is minified and the filenames include the hashes.\
-Your app is ready to be deployed!
+- On the main page, enter the full name of a contact in the search box.
+- As you type, the app will display a dropdown menu with matching contacts.
+- Select a contact from the dropdown menu or continue typing to refine the search.
+- Click the "Search" button to view the contact details.
+- If no contacts are found, a message will be displayed.
+- To perform a new search, click the "Go Back" button.
 
-See the section about [deployment](https://facebook.github.io/create-react-app/docs/deployment) for more information.
+## Contributing
 
-### `npm run eject`
-
-**Note: this is a one-way operation. Once you `eject`, you can't go back!**
-
-If you aren't satisfied with the build tool and configuration choices, you can `eject` at any time. This command will remove the single build dependency from your project.
-
-Instead, it will copy all the configuration files and the transitive dependencies (webpack, Babel, ESLint, etc) right into your project so you have full control over them. All of the commands except `eject` will still work, but they will point to the copied scripts so you can tweak them. At this point you're on your own.
-
-You don't have to ever use `eject`. The curated feature set is suitable for small and middle deployments, and you shouldn't feel obligated to use this feature. However we understand that this tool wouldn't be useful if you couldn't customize it when you are ready for it.
-
-## Learn More
-
-You can learn more in the [Create React App documentation](https://facebook.github.io/create-react-app/docs/getting-started).
-
-To learn React, check out the [React documentation](https://reactjs.org/).
-
-### Code Splitting
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/code-splitting](https://facebook.github.io/create-react-app/docs/code-splitting)
-
-### Analyzing the Bundle Size
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/analyzing-the-bundle-size](https://facebook.github.io/create-react-app/docs/analyzing-the-bundle-size)
-
-### Making a Progressive Web App
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/making-a-progressive-web-app](https://facebook.github.io/create-react-app/docs/making-a-progressive-web-app)
-
-### Advanced Configuration
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/advanced-configuration](https://facebook.github.io/create-react-app/docs/advanced-configuration)
-
-### Deployment
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/deployment](https://facebook.github.io/create-react-app/docs/deployment)
-
-### `npm run build` fails to minify
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify](https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify)
+Contributions to the Contact-Finder are welcome! If you find any issues or have suggestions for improvements, please open an issue or submit a pull request.
--- src/App.js
+++ src/App.js
@@ -1,23 +1,146 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import { Table, Button, Form } from "react-bootstrap";
+import "./App.css";
 
 function App() {
+  // State variables
+  const [showNoContacts, setShowNoContacts] = useState(false); // Flag to show "No contacts found" message
+  const [contacts, setContacts] = useState([]); // Array to store all contacts
+  const [searchTerm, setSearchTerm] = useState(""); // Search term entered by the user
+  const [filteredContacts, setFilteredContacts] = useState([]); // Filtered contacts based on the search term
+  const [noContactsFound, setNoContactsFound] = useState(false); // Flag to indicate if no contacts are found
+  const [contactNames, setContactNames] = useState([]); // Array to store contact names for filtering
+  const [searchButtonClicked, setSearchButtonClicked] = useState(false); // Flag to indicate if search button is clicked
+
+  useEffect(() => {
+    // Fetch contacts data when component mounts
+    fetchContacts();
+  }, []);
+
+  // Fetch contacts data from the API
+  const fetchContacts = () => {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((response) => response.json())
+      .then((data) => {
+        setContacts(data);
+        const names = data.map((contact) => contact.name);
+        setContactNames(names);
+      })
+      .catch((error) => {
+        console.log("An error occurred while fetching contacts:", error);
+      });
+  };
+
+  // Handle form submission for searching contacts
+  const searchContacts = (e) => {
+    e.preventDefault();
+    const exactContacts = contacts.filter(
+      (contact) => contact.name.toLowerCase() === searchTerm.toLowerCase()
+    );
+    setFilteredContacts(exactContacts);
+    setShowNoContacts(exactContacts.length === 0 && searchTerm !== "");
+    setSearchTerm("");
+    setSearchButtonClicked(true);
+  };
+
+  // Handle input change for filtering contact names
+  const handleInputChange = (e) => {
+    setSearchTerm(e.target.value);
+    const filteredNames = contactNames.filter((name) =>
+      name.toLowerCase().startsWith(e.target.value.toLowerCase())
+    );
+    setFilteredContacts(filteredNames);
+  };
+
+  // Handle click on a contact name to set it in the search input
+  const handleContactClick = (name) => {
+    setSearchTerm(name);
+    setFilteredContacts([]);
+  };
+
+  // Show the main page and reset the search inputs and results
+  const showMainPage = () => {
+    setSearchTerm("");
+    setFilteredContacts([]);
+    setSearchButtonClicked(false);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h1 className="text-center mb-4">Contacts</h1>
+
+      <div className="main-page">
+        <Form onSubmit={searchContacts} className="text-center">
+          <Form.Group controlId="search-input">
+            <Form.Control
+              type="text"
+              value={searchTerm}
+              onChange={handleInputChange}
+              placeholder="Enter the full name"
+              className="search-input"
+              autoComplete="off"
+              list="contact-names"
+            />
+            {/* Render datalist only when search term is entered */}
+            {searchTerm && (
+              <datalist id="contact-names">
+                {filteredContacts.map((name) => (
+                  // Handle click on a contact name from the list
+                  <option
+                    key={name}
+                    value={name}
+                    onClick={() => handleContactClick(name)}
+                  />
+                ))}
+              </datalist>
+            )}
+            <Button variant="primary" type="submit">
+              Search
+            </Button>
+          </Form.Group>
+        </Form>
+      </div>
+
+      {/* Show "No contacts found" message when applicable */}
+      {searchButtonClicked && showNoContacts && (
+        <div className="text-center mt-4 no-contacts">
+          <p>No contacts found.</p>
+          <Button variant="primary" onClick={showMainPage}>
+            Go Back
+          </Button>
+        </div>
+      )}
+
+      {/* Show filtered contacts when search button is clicked */}
+      {searchButtonClicked && filteredContacts.length > 0 && (
+        <div className="contact-details-page">
+          <h2 className="text-center mt-4">Contact Details</h2>
+          <Table striped bordered responsive>
+            <thead>
+              <tr>
+                <th>Full name</th>
+                <th>Email</th>
+                <th>Phone</th>
+              </tr>
+            </thead>
+            <tbody>
+              {filteredContacts.map((contact) => (
+                <tr key={contact.id}>
+                  <td>{contact.name}</td>
+                  <td>{contact.email}</td>
+                  <td>{contact.phone}</td>
+                </tr>
+              ))}
+            </tbody>
+          </Table>
+
+          <div className="text-center">
+            <Button variant="primary" onClick={showMainPage}>
+              Go Back
+            </Button>
+          </div>
+        </div>
+      )}
     </div>
   );
 }
